622a6b1ef9268bac46bb8b6bef61758cdddfec40
tatsu <tatsu1224@gmail.com>
2022-08-15 14:34:54 +0900

    Update.

---
+++ Sample_LinearProgramming.R
@@ -0,0 +1,17 @@
+#線形計画法のサンプル
+install.packages('lpSolve')
+library(lpSolve)
+
+#下記の例では、
+#x_1 + x_2 を、
+#x_1 + 2x_2 <= 4
+#2x_1 + x_2 <= 4
+#x_1 , x_2 >= 0 の条件下で最大化している。
+
+f_obj <- c(1,1) #目的関数の係数
+f_con <- matrix(c(1,2,2,1),ncol=2) #制約条件を行列で
+f_dir <- c("<=","<=") #不等号?
+f_rhs <- c(4,4) # 不等号の右側
+
+lp("max",f_obj,f_con,f_dir,f_rhs)
+lp("max",f_obj,f_con,f_dir,f_rhs)$solution
--- scripts/test_caret.R
+++ scripts/test_caret.R
@@ -1,15 +1,15 @@
-install.packages("pmml")
-install.packages("readr")
-install.packages("dplyr")
-install.packages("summarytools")
-install.packages("RANN")
-install.packages("gower")
-install.packages("ModelMetrics")
-install.packages("hardhat")
-install.packages("tidyverse")
-install.packages("skimr")
-install.packages("doParallel")
-install.packages("caret", dependencies = c("Depends", "Suggests"))
+#install.packages("pmml")
+#install.packages("readr")
+#install.packages("dplyr")
+#install.packages("summarytools")
+#install.packages("RANN")
+#install.packages("gower")
+#install.packages("ModelMetrics")
+#install.packages("hardhat")
+#install.packages("tidyverse")
+#install.packages("skimr")
+#install.packages("doParallel")
+#install.packages("caret", dependencies = c("Depends", "Suggests"))
 
 
 library(caret)
@@ -17,6 +17,11 @@ library(readr)
 library(skimr)
 library(summarytools)
 library(tidyverse)
+library(doParallel)
+
+#並列計算指定
+cl <- makePSOCKcluster(4)
+registerDoParallel(cl)
 
 url = "https://www.salesanalytics.co.jp/wpv6"
 df_diamond = read_csv(url) 
@@ -45,10 +50,31 @@ range_model <- preProcess(x_train, method = c("center","scale"))
 x_train <- predict(range_model, newdata = x_train)
 x_test <- predict(range_model, newdata = x_test)
 
-#ダミー変数化
+#ダミー変数化(One-hot-encoding)
 dummies_model <- dummyVars(~.,data = x_train, fullRank=TRUE)
 x_train <- data.frame(predict(dummies_model, newdata = x_train))
 x_test <- data.frame(predict(dummies_model, newdata = x_test))
-#test
+#一旦確認
 skim(x_train)
 skim(x_test)
+help(train)
+#ロバスト線形回帰
+model_rlm <- train(
+  price ~ .,
+  data = cbind(y_train, x_train),
+  method = 'rlm', 
+  tuneGrid = expand.grid(intercept=c('TRUE','FALSE'),
+                         psi=c('huber','hampel','bisquare')),
+  trControl = trainControl(method = 'cv', 
+                           number = 10)
+)
+
+#結果の確認
+model_rlm
+varImp(model_rlm, scale=TRUE)
+#最終モデルの係数確認
+coef(model_rlm$finalModel)
+
+#テストデータへのあてはめ
+pred_rlm <- predict(model_rlm,x_test)
+postResample(pred=pred_rlm, obs=y_test$price)
